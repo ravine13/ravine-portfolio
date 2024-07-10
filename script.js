4d440db8b03423f717afb434ef455bd65b0aa8ef
ravine13 <kuriaderek@gmail.com>
2024-07-10 20:37:45 +0300

    new: project links updated

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const loaderContainer = document.querySelector('.preloader');
+    const loaderContainers = document.querySelectorAll('.loader');
     const pageContent = document.querySelector('#page-content');
 
-    window.addEventListener('load', () => {
-        loaderContainer.classList.add('hidden');
-        pageContent.classList.add('visible');
-    });
 });
-
-function toggleMenu() {
-    const menu = document.querySelector('.menu-links');
-    const icons = document.querySelector('.hamburger-icon');
-    menu.classList.toggle('open');
-    icons.classList.toggle('open');
-}
